e53501fab619be141ecc0de98e3390eaa42a909b
Layne Bennett <laynebennett64@gmail.com>
2025-03-24 08:40:17 -0500

    Update button.c

--- Lab4/button.c
+++ Lab4/button.c
@@ -28,34 +28,11 @@ void button_init() {
 		return;
 	}
 
-	// delete warning after implementing
-
-
-	// Reading: To initialize and configure GPIO PORTE, visit pg. 656 in the
-	// Tiva datasheet.
-
-	// Follow steps in 10.3 for initialization and configuration. Some steps
-	// have been outlined below.
-
-	// Ignore all other steps in initialization and configuration that are not
-	// listed below. You will learn more about additional steps in a later lab.
-
-	// 1) Turn on PORTE system clock, do not modify other clock enables
-	//SYSCTL_RCGCGPIO_R |=
-	// You may need to add a delay here of several clock cycles for the clock to start, e.g., execute a simple dummy assignment statement, such as "long delay = SYSCTL_RCGCGPIO_R".
-  // Instead, use the PRGPIO register and busy-wait on the peripheral ready bit for PORTE.
-  // while ((SYSCTL_PRGPIO_R & 0x??) == 0) {};
-	// 2) Set the buttons as inputs, do not modify other PORTE wires
-	// GPIO_PORTE_DIR_R &=
-
-	// 3) Enable digital functionality for button inputs,
-	//    do not modify other PORTE enables
-	// GPIO_PORTE_DEN_R |=
-
-	SYSCTL_RCGCGPIO_R |= 0b10000; //0x10
+	
+	SYSCTL_RCGCGPIO_R |= 0b10000; //0x10 start register clock
 	while ((SYSCTL_PRGPIO_R & 0b10000) == 0) {};
-	GPIO_PORTE_DIR_R &= 0b11110000;
-	GPIO_PORTE_DEN_R |= 0b00001111;
+	GPIO_PORTE_DIR_R &= 0b11110000; //set direction of buttons
+	GPIO_PORTE_DEN_R |= 0b00001111; //enable register
 
 	initialized = 1;
 }
@@ -68,42 +45,9 @@ void button_init() {
  */
 uint8_t button_getButton() {
 
-		// delete warning after implementing
-
-	//
-	// DELETE ME - How bitmasking works
-	// ----------------------------------------
-	// In embedded programming, often we only care about one or a few bits in a piece of
-	// data.  There are several bitwise operators that we can apply to data in order
-	// to "mask" the bits that we don't care about.
-	//
-	//	| = bitwise OR		& = bitwise AND		^ = bitwise XOR		~ = bitwise NOT
-	//		  << x = shift left by x bits		 >> x = shift right by x bits
-	//
-	// Let's say we want to know if push button 3 (Sw3) is pushed.  Sw3 is bit 2 of GPIO_PORTE_DATA_R.
-	// Since push buttons are high (1) initially, and low (0) if pushed, PORTE should
-	// look like:
-	// GPIO_PORTE_DATA_R => 0b???? ?0?? if Sw3 is pushed
-	// GPIO_PORTE_DATA_R => 0b???? ?1?? if Sw3 is not pushed
-	//
-	// We want to only look at bit 2 and mask the other 7 bits:
-	//
-	// Bitwise AND:
-	// (GPIO_PORTE_DATA_R & 0b0000 0100) => 0b0000 0000 if Sw3 is pushed
-	// (GPIO_PORTE_DATA_R & 0b0000 0100) => 0b0000 0100 if Sw3 is not pushed
-	//
-	// Bitwise OR:
-	// (GPIO_PORTE_DATA_R | 0b1111 1011) => 0b1111 1011 if Sw3 is pushed
-	// (GPIO_PORTE_DATA_R | 0b1111 1011) => 0b1111 1111 if Sw3 is not pushed
-	//
-	// Other techniques (Shifting and bitwise AND)
-	// ((GPIO_PORTE_DATA_R >> 2) & 1) => 0 if Sw3 is pushed
-	// ((GPIO_PORTE_DATA_R >> 2) & 1) => 1 if Sw3 is not pushed
-
-	// TODO: Write code below -- Return the rightmost button position pressed
 
 
-    if((GPIO_PORTE_DATA_R & 0b00001000) == 0x0){
+    if((GPIO_PORTE_DATA_R & 0b00001000) == 0x0){ //returns 1 2 3 and 4 for each button pressed
         return 0b00000100;
     }else if((GPIO_PORTE_DATA_R & 0b00000100) == 0x0){
         return 0b00000011;
